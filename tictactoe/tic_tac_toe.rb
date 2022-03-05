dda376c869c79a85cefa31986efa93e6ccfcaa2b
Eiji Mine <eijimine@gmail.com>
2022-03-05 18:21:40 -0500

    5x5 row winner refactored

--- tictactoe/tic_tac_toe.rb
+++ tictactoe/tic_tac_toe.rb
@@ -4,80 +4,87 @@ class TicTacToe
   end
 
   def winner
-    row1 = @board[0]
-    row2 = @board[1]
-    row3 = @board[2]
+    # row1 = @board[0]
+    # row2 = @board[1]
+    # row3 = @board[2]
+
 
     # row checks
 
-    if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
-      return "o"
+    @board.each do |row|
+      ['o', 'x'].each do |play|
+        return play if row.all?(play)
+      end
     end
 
-    if row2[0] == "o" && row2[1] == "o" && row2[2] == "o"
-      return "o"
-    end
+    # if row1[0] == "o" && row1[1] == "o" && row1[2] == "o"
+    #   return "o"
+    # end
 
-    if row3[0] == "o" && row3[1] == "o" && row3[2] == "o"
-      return "o"
-    end
+    # if row2[0] == "o" && row2[1] == "o" && row2[2] == "o"
+    #   return "o"
+    # end
 
-    if row1[0] == "x" && row1[1] == "x" && row1[2] == "x"
-      return "x"
-    end
+    # if row3[0] == "o" && row3[1] == "o" && row3[2] == "o"
+    #   return "o"
+    # end
 
-    if row2[0] == "x" && row2[1] == "x" && row2[2] == "x"
-      return "x"
-    end
+    # if row1[0] == "x" && row1[1] == "x" && row1[2] == "x"
+    #   return "x"
+    # end
 
-    if row3[0] == "x" && row3[1] == "x" && row3[2] == "x"
-      return "x"
-    end
+    # if row2[0] == "x" && row2[1] == "x" && row2[2] == "x"
+    #   return "x"
+    # end
+
+    # if row3[0] == "x" && row3[1] == "x" && row3[2] == "x"
+    #   return "x"
+    # end
 
     # column checks
 
-    if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
-      return "o"
-    end
+    # if row1[0] == "o" && row2[0] == "o" && row3[0] == "o"
+    #   return "o"
+    # end
 
-    if row1[1] == "o" && row2[1] == "o" && row3[1] == "o"
-      return "o"
-    end
+    # if row1[1] == "o" && row2[1] == "o" && row3[1] == "o"
+    #   return "o"
+    # end
 
-    if row1[2] == "o" && row2[2] == "o" && row3[2] == "o"
-      return "o"
-    end
+    # if row1[2] == "o" && row2[2] == "o" && row3[2] == "o"
+    #   return "o"
+    # end
 
-    if row1[0] == "x" && row2[0] == "x" && row3[0] == "x"
-      return "x"
-    end
+    # if row1[0] == "x" && row2[0] == "x" && row3[0] == "x"
+    #   return "x"
+    # end
 
-    if row1[1] == "x" && row2[1] == "x" && row3[1] == "x"
-      return "x"
-    end
+    # if row1[1] == "x" && row2[1] == "x" && row3[1] == "x"
+    #   return "x"
+    # end
 
-    if row1[2] == "x" && row2[2] == "x" && row3[2] == "x"
-      return "x"
-    end
+    # if row1[2] == "x" && row2[2] == "x" && row3[2] == "x"
+    #   return "x"
+    # end
 
-    # diagonal checks
+    # # diagonal checks
 
-    if row1[0] == "o" && row2[1] == "o" && row3[2] == "o"
-      return "o"
-    end
+    # if row1[0] == "o" && row2[1] == "o" && row3[2] == "o"
+    #   return "o"
+    # end
 
-    if row1[2] == "o" && row2[1] == "o" && row3[0] == "o"
-      return "o"
-    end
+    # if row1[2] == "o" && row2[1] == "o" && row3[0] == "o"
+    #   return "o"
+    # end
 
-    if row1[0] == "x" && row2[1] == "x" && row3[2] == "x"
-      return "x"
-    end
+    # if row1[0] == "x" && row2[1] == "x" && row3[2] == "x"
+    #   return "x"
+    # end
 
-    if row1[2] == "x" && row2[1] == "x" && row3[0] == "x"
-      return "x"
-    end
+    # if row1[2] == "x" && row2[1] == "x" && row3[0] == "x"
+    #   return "x"
+    # end
 
-    return "draw"
+    # return "draw"
   end
 end
